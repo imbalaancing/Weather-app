e0a2e0d09b6955261310faff280bd63b6e6f817e
nicky <nikkalaancing@gmail.com>
2022-11-21 20:54:26 +0700

    adding project styles

--- src/App.vue
+++ src/App.vue
@@ -29,5 +29,32 @@
 }
 
 .wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+
+.wrapper input:focus {
+  border-bottom-color: #6e2de7;
+}
+
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
