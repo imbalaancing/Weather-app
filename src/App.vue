71d92a10f2a5265b227db07360caa285fde32291
nicky <nikkalaancing@gmail.com>
2022-11-21 20:44:47 +0700

    adding a project structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <script></script>
 
-<template></template>
+<template>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в вашем городе:</p>
+    <input type="text" placeholder="Введите город" />
+    <button>Запросить погоду</button>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background: #1f0f24;
+  color: #fff;
+  text-align: center;
+}
+
+.wrapper h1 {
+  margin-top: 50px;
+}
+
+.wrapper p {
+  margin-top: 20px;
+}
+
+.wrapper input {
+}
+</style>
